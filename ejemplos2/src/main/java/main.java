60666c15ee7bfa8b94525e91d13858679baf4eba
juban1234 <alexzo8677gmail.com>
2024-07-19 14:34:13 -0500

    actualizacion

---
+++ ejemplos2/src/main/java/main.java
@@ -0,0 +1,10 @@
+
+public class main {
+    public static void main(String[] args) { 
+        int x=4;        
+        int y=5;
+        
+        boolean menorque=(x<y);
+        System.out.println("4<5: "+menorque);
+    }
+}
