baeac418900f6b321dfe8abe4722bfddeeea0752
juban1234 <alexzo8677gmail.com>
2024-07-19 14:41:10 -0500

    actualizacion

--- ejemplos2/src/main/java/main.java
+++ ejemplos2/src/main/java/main.java
@@ -6,5 +6,20 @@ public class main {
         
         boolean menorque=(x<y);
         System.out.println("4<5: "+menorque);
+        
+        boolean mayorque=(x<y);
+        System.out.println("4<5: "+mayorque);
+        
+        boolean menoroigualque=(x<y);
+        System.out.println("4<5: "+menoroigualque);
+        
+        boolean mayoroigualque=(x<y);
+        System.out.println("4<5: "+mayoroigualque);
+        
+        boolean iguala=(x==y);
+        System.out.println("x!=y: "+iguala);
+        
+        boolean diferentede=(x!=y);
+        System.out.println("4!=4: "+diferentede);
     }
 }
